0ce44d0c5c7f57b25c1275527db9099656d9283e
Joseph Thawn <pauthawn@Josephs-MacBook-Pro.local>
2022-08-18 23:36:47 -0500

    Welcome to Emazon

--- pages/_document.js
+++ pages/_document.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Document, { Head, Html, Main, NextScript } from "next/document";
-import createEmotionServer from "@emotion/server/crate-instance";
+import createEmotionServer from "@emotion/server/create-instance";
+import createCache from "@emotion/cache";
 
 export default class MyDocument extends Document {
   render() {
@@ -9,14 +10,42 @@ export default class MyDocument extends Document {
         <Head>
           <link
             rel="stylesheet"
-            href="https;//fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
+            href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
           />
-          <body>
-            <Main />
-            <NextScript />
-          </body>
         </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
       </Html>
     );
   }
 }
+
+MyDocument.getInitialProps = async (ctx) => {
+  const originalRenderPage = ctx.renderPage;
+  const cache = createCache({ key: "css" });
+  const { extractCriticalToChunks } = createEmotionServer(cache);
+  ctx.renderPage = () =>
+    originalRenderPage({
+      // eslint-disable-next-line react/display-name
+      enhanceApp: (App) => (props) => <App emotionCache={cache} {...props} />,
+    });
+  const initialProps = await Document.getInitialProps(ctx);
+  const emotionStyles = extractCriticalToChunks(initialProps.html);
+  const emotionStyleTags = emotionStyles.styles.map((style) => (
+    <style
+      data-emotion={`${style.key} ${style.ids.join(" ")}`}
+      key={style.key}
+      // eslint-disable-next-line react/no-danger
+      dangerouslySetInnerHTML={{ __html: style.css }}
+    />
+  ));
+  return {
+    ...initialProps,
+    styles: [
+      ...React.Children.toArray(initialProps.styles),
+      ...emotionStyleTags,
+    ],
+  };
+};
